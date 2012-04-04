189f0860e236c76af337e275b85443e7aa2d828a
liaozhenhua <314773862@qq.com>
2012-04-04 18:06:53 +0800

    firt commit rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sample::Application.config.secret_token = '7c7eb4c7eb7d2831d8516626f23daf1c34d9889615b08c17458f4fcde1326901f1634dd84d009c181dab0506f1687ff1c3fceb13ddcfdb3194d5428e86917965'
